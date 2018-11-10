f93d24cea6f88fe03725042c3f079a698ddadd90
Riel <s.v.riel@student.tue.nl>
2018-11-10 15:13:51 +0100

    Combine GUI with logic code. Bugs present.

--- Snake/GUI.cpp
+++ Snake/GUI.cpp
@@ -3,15 +3,92 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
-//start the application, don't do any stuff here
-void main(cli::array<String^>^ args)
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
+namespace Snake {
 
-	Snake::GUI gui;
- Timer^ timer = gcnew Timer();
- timer->Tick += gcnew EventHandler(%gui,&Snake::GUI::UpdateScreen);
+  // Update the screen on a frame clock trigger
+  void GUI::FrameUpdate(Object^ sender, EventArgs^ e)
+  {
 
-	Application::Run(%gui);
+    this->game->step(currentDirection);
+
+    // If body parts have been added in this step, add graphic objects for them
+    unsigned int nrNewParts = this->game->getBody().size() - this->graphics->Count;
+    for (unsigned int i = 0; i < nrNewParts; i++)
+    {
+      PictureBox^ newObj = gcnew PictureBox();
+      newObj->Image = gcnew Bitmap(Image::FromFile("./Resources/box.bmp"), gridSize.X, gridSize.Y);
+      this->Controls->Add(newObj);
+      this->graphics->Add(newObj);
+    }
+
+    // Update head position
+    this->graphics[0]->Location = Point(
+      this->game->getBody()[0].x * gridSize.X,
+      this->game->getBody()[0].y * gridSize.Y);
+
+    this->graphics[0]->Update();
+    this->graphics[0]->Refresh();
+
+    /*if (currentDirection == 0)
+    {
+      this->graphics[0]->Location = Point(
+        this->graphics[0]->Location.X + gridSize.X, 
+        this->graphics[0]->Location.Y);
+    }
+    else if (currentDirection == 1)
+    {
+      this->graphics[0]->Location = Point(
+        this->graphics[0]->Location.X, 
+        this->graphics[0]->Location.Y - gridSize.Y);
+    }
+    else if (currentDirection == 2)
+    {
+      this->graphics[0]->Location = Point(
+        this->graphics[0]->Location.X - gridSize.X, 
+        this->graphics[0]->Location.Y);
+    }
+    else if (currentDirection == 3)
+    {
+      this->graphics[0]->Location = Point(
+        this->graphics[0]->Location.X, 
+        this->graphics[0]->Location.Y + gridSize.Y);
+    }
+    this->graphics[0]->Update();
+    this->graphics[0]->Refresh();
+    */
+
+    
+  }
+
+  // Handle keyboard input
+  void GUI::KeyHandler(Object^ sender, KeyEventArgs^ e)
+  {
+    switch (e->KeyCode)
+    {
+    case (Keys::D): // Right
+      this->currentDirection = 0;
+      break;
+    case (Keys::W): // Up
+      this->currentDirection = 1;
+      break;
+    case (Keys::A): // Left
+      this->currentDirection = 2;
+      break;
+    case (Keys::S): // Right
+      this->currentDirection = 3;
+      break;
+    }
+
+  }
+
+    // Start the application
+    void main(cli::array<String^>^ args)
+  {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Snake::GUI gui;
+    Application::Run(%gui);
+  }
+
 }
--- Snake/GUI.h
+++ Snake/GUI.h
@@ -1,176 +1,117 @@
 #pragma once
 
-#include "GameObject.h"
-#include <stdio.h>
+#include "Game.h"
 #include <iostream>
 
 namespace Snake {
 
- using namespace System;
- using namespace System::Collections::Generic;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+  using namespace System;
+  using namespace System::Collections::Generic;
+  using namespace System::ComponentModel;
+  using namespace System::Collections;
+  using namespace System::Windows::Forms;
+  using namespace System::Data;
+  using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for GUI
-	/// </summary>
-	public ref class GUI : public System::Windows::Forms::Form
-	{
-	public:
-		GUI(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-
-   this->KeyPreview = true;
-   this->KeyDown += gcnew KeyEventHandler(KeyHandler);
-
-   graphics = gcnew List<PictureBox^>();
-
-   PictureBox^ newObj = gcnew PictureBox();
-   newObj->Location = Point(100, 100);
-   newObj->Image = gcnew Bitmap("./Resources/box.bmp");
-   this->Controls->Add(newObj);
-
-   graphics->Add(newObj);
-   }
-
-  void UpdateScreen(Object^ sender, EventArgs^ e)
+  /// <summary>
+  /// Summary for GUI
+  /// </summary>
+  public ref class GUI : public System::Windows::Forms::Form
   {
-    if (currentDirection == 0)
+  public:
+    GUI(void)
     {
-      this->graphics[0]->Location = Point(this->graphics[0]->Location.X+gridSize, this->graphics[0]->Location.Y);
+      // Initialize all GUI components
+      InitializeComponent();
+
+      // Setup grid size to fit the window
+      this->gridSize = Point(
+        windowSize.X / gridDims.X,
+        windowSize.Y / gridDims.Y);
+
+      // Setup game
+      game = new Game(this->gridDims.X, this->gridDims.Y);
+      
+      // Setup graphics
+      graphics = gcnew List<PictureBox^>();
     }
-    else if (currentDirection == 1)
+
+  protected:
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    ~GUI()
     {
-      this->graphics[0]->Location = Point(this->graphics[0]->Location.X, this->graphics[0]->Location.Y - gridSize);
-    }
-    else if (currentDirection == 2)
-    {
-      this->graphics[0]->Location = Point(this->graphics[0]->Location.X - gridSize, this->graphics[0]->Location.Y);
-    }
-    else if (currentDirection == 3)
-    {
-      this->graphics[0]->Location = Point(this->graphics[0]->Location.X, this->graphics[0]->Location.Y + gridSize);
+      if (components)
+      {
+        delete components;
+      }
     }
 
-    std::cout << "Moved to " << this->graphics[0]->Location.X << "," << this->graphics[0]->Location.X << std::endl;
-    this->graphics[0]->Update();
-    this->graphics[0]->Refresh();
-  }
+  private: 
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~GUI()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
- private: System::ComponentModel::IContainer^  components;
- protected:
+    // Update the screen at a frame update
+    void FrameUpdate(Object^ sender, EventArgs^ e);
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
+    // Handle all keyboard input
+    void KeyHandler(Object^ sender, KeyEventArgs^ e);
+    
+    System::ComponentModel::IContainer^  components;
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
 
+    // The timer that performs game update
+    System::Windows::Forms::Timer^  gameTimer;
+    unsigned int gameTickPeriod = 200; // The period of the game timer, in ms 
 
-		/// The timer that performs screen update
-  unsigned int gameTickPeriod = 200;
+    // The main game object
+    Game* game;
 
-  // The graphics objects present in the GUI
-  List<PictureBox^>^ graphics;
+    // All graphics objects present in the GUI
+    List<PictureBox^>^ graphics;
 
-  // The current direction input
-  unsigned int currentDirection = 0;
- private: System::Windows::Forms::Timer^  timer1;
+    // The current direction input
+    unsigned int currentDirection = 0;
 
-  const int gridSize = 50;
+    // GUI Sizes
+    Point windowSize = Point(640, 640); // Window size in pixels
+    Point gridDims = Point(20, 20);     // Grid dimensions
+    Point gridSize;                     // Grid size, computed to fit the window
 
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-    this->components = (gcnew System::ComponentModel::Container());
-    this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
-    this->SuspendLayout();
-    // 
-    // timer1
-    // 
-    this->timer1->Tick += gcnew System::EventHandler(this, &GUI::UpdateScreen);
-    this->timer1->Interval = gameTickPeriod;
-    this->timer1->Start();
-    // 
-    // GUI
-    // 
-    this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-    this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-    this->ClientSize = System::Drawing::Size(480, 480);
-    this->Name = L"GUI";
-    this->Text = L"GUI";
-    this->ResumeLayout(false);
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    void InitializeComponent(void)
+    {
+      this->components = (gcnew System::ComponentModel::Container());
+      this->gameTimer = (gcnew System::Windows::Forms::Timer(this->components));
+      this->SuspendLayout();
+      // 
+      // timer1
+      // 
+      this->gameTimer->Tick += gcnew System::EventHandler(this, &GUI::FrameUpdate);
+      this->gameTimer->Interval = gameTickPeriod;
+      this->gameTimer->Start();
+      // 
+      // GUI
+      // 
+      this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+      this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+      this->ClientSize = System::Drawing::Size(480, 480);
+      this->Name = L"GUI";
+      this->Text = L"GUI";
+      this->ResumeLayout(false);
 
-  }
+      // Key Handling
+      this->KeyPreview = true;
+      this->KeyDown += gcnew KeyEventHandler(this, &GUI::KeyHandler);
+
+    }
 #pragma endregion
 
-  static void KeyHandler(Object^ sender, KeyEventArgs^ e)
-  {
-    GUI^ gui = (GUI^)sender;
-    
-    if (e->KeyCode == System::Windows::Forms::Keys::D)
-    {
-      // Up
-      gui->currentDirection = 0;
-    }
-    else if (e->KeyCode == System::Windows::Forms::Keys::W)
-    {
-      // Up
-      gui->currentDirection = 1;
-    }
-    else if (e->KeyCode == System::Windows::Forms::Keys::A)
-    {
-      // Up
-      gui->currentDirection = 2;
-    }
-    else if (e->KeyCode == System::Windows::Forms::Keys::S)
-    {
-      // Up
-      gui->currentDirection = 3;
-    }
-    
-    //test
-    /*
-    Point newLocation = Point(gui->graphics[0]->Location);
-    if (e->KeyCode == System::Windows::Forms::Keys::W)
-      newLocation.Y -= 10;
-    else if (e->KeyCode == System::Windows::Forms::Keys::A)
-      newLocation.X -= 10;
-    else if (e->KeyCode == System::Windows::Forms::Keys::S)
-      newLocation.Y += 10;
-    else if (e->KeyCode == System::Windows::Forms::Keys::D)
-      newLocation.X += 10;
-
-    if (!newLocation.Equals(gui->graphics[0]->Location))
-    {
-      gui->graphics[0]->Location = newLocation;
-      gui->graphics[0]->Update();
-      gui->graphics[0]->Refresh();
-    }
-    */
-  }
-
-	};
+  };
 }
